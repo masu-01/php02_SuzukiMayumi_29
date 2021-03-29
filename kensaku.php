68c0d51ae35ea174b86ceb33e0326da472af155d
ma-su <l0meet07@gmail.com>
2021-03-30 01:03:51 +0900

    検索結果エラー解消せず

--- kensaku.php
+++ kensaku.php
@@ -10,13 +10,14 @@ try {
     $pdo = new PDO('mysql:dbname=book_list;charset=utf8;host=localhost','root','root');
 
     //  検索するSQL文
-    $sql = "select * from book_data where author = :keyword";
+    // $sql = 'select * from book_data where author = :keyword';
+
 
     // プリペアドステートメントを作成
-	$stmt = $pdo->prepare($sql);
+	$stmt = $pdo->prepare("SELECT * FROM book_data WHERE author = :keyword");
 
     // プレースホルダと変数をバインド
-	$stmt -> bindParam(":keyword",$keyword);
+	$stmt -> bindParam(":keyword", $keyword, PDO::PARAM_STR);
 	$stmt -> execute(); //実行
 
 	// データを取得
@@ -26,38 +27,40 @@ try {
     exit('DBConnectError'.$e->getMessage());
 }
 
-
-// try-catch
-// try{
-// 	// データベースへの接続を表すPDOインスタンスを生成
-// 	$pdo = new PDO($dsn,$username,$password);
-
-	//  SQL文 :idは、名前付きプレースホルダ
-	$sql = "select * from syain where id = :id";
-
-	// プリペアドステートメントを作成
-	$stmt = $pdo->prepare($sql);
-
-	// プレースホルダと変数をバインド
-	$stmt -> bindParam(":id",$id);
-	$stmt -> execute(); //実行
-
-	// データを取得
-	$rec = $stmt->fetch(PDO::FETCH_ASSOC);
-
-	// 接続を閉じる
-	//$pdo = null; スクリプト終了時に自動で切断されるので不要
-}catch (PDOException $e) {
-	// UTF8に文字エンコーディングを変換します
-	exit(mb_convert_encoding($e->getMessage(),'UTF-8','SJIS-win'));   
-}
-function escape1($str)
-{
-    return htmlspecialchars($str,ENT_QUOTES,'UTF-8');
-}
-
-
 ?>
 
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>検索結果</title>
+<link rel="stylesheet" href="css/range.css">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<style>div{padding: 10px;font-size:16px;}</style>
+</head>
+<body id="main">
+<!-- Head[Start] -->
+<header>
+    <nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+        <a class="navbar-brand" href="index.php">書籍登録フォームはこちら</a>
+        </div>
+        <div class="navbar-header">
+        <a class="navbar-brand" href="select copy.php">一覧に戻る</a>
+        </div>
+    </div>
+    </nav>
+</header>
+<!-- Head[End] -->
 
-//検索したい
+<!-- Main[Start] -->
+<div>
+    <div class="container jumbotron"><?= $rec ?></div>
+</div>
+<!-- Main[End] -->
+
+</body>
+</html>
